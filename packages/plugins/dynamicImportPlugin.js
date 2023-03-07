0d0d7c6e62ccf4c10e49062ea4476deebda2eceb
jaluik <cqzhengmouren@gmail.com>
2023-03-07 11:14:12 +0800

    feat: 项目调整

---
+++ packages/plugins/dynamicImportPlugin.js
@@ -0,0 +1,63 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+module.exports = class TestWebpackPlugin {
+  constructor() {
+    this.commonHisSrcPath = this.getCommonHisSrcPath();
+    this.tryExt = ['ts', 'tsx'];
+  }
+
+  apply(compiler) {
+    compiler.hooks.normalModuleFactory.tap(
+      'DynamicImportPlugin',
+      (normalModuleFactory) => {
+        normalModuleFactory.hooks.beforeResolve.tap(
+          'DynamicImportPlugin',
+          (result) => {
+            if (!result) return;
+            if (result.context.includes('node_modules')) {
+              return result;
+            }
+            const regText = /(.*packages.*src).*/;
+            if (result.request.includes('@/') && regText.test(result.context)) {
+              const pathDir = result.context.match(regText);
+              const rootPath = pathDir[1];
+              if (rootPath) {
+                const newPath = path.resolve(
+                  rootPath,
+                  result.request.replace('@/', ''),
+                );
+                const commonHisPath = path.resolve(
+                  this.commonHisSrcPath,
+                  result.request.replace('@/', ''),
+                );
+                if (this.tryExist(newPath)) {
+                  result.request = newPath;
+                } else {
+                  result.request = path.resolve(commonHisPath);
+                }
+              }
+            }
+            return result;
+          },
+        );
+      },
+    );
+  }
+
+  getCommonHisSrcPath() {
+    return path.resolve(__dirname, '../commonHis/src');
+  }
+
+  tryExist(pathDir) {
+    return this.tryExt.some((ext) => {
+      if (
+        fs.existsSync(path.resolve(pathDir, ext)) ||
+        fs.existsSync(path.resolve(pathDir, `index.${ext}`))
+      ) {
+        return true;
+      }
+      return false;
+    });
+  }
+};
