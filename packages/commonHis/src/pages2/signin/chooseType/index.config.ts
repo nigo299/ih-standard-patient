e49d357634e0e384520cbb9466481e58b668c6f2
jaluik <cqzhengmouren@gmail.com>
2023-03-09 14:05:51 +0800

    fix: 修复项目报错

--- packages/commonHis/src/pages2/signin/chooseType/index.config.ts
+++ packages/commonHis/src/pages2/signin/chooseType/index.config.ts
@@ -1,8 +1,6 @@
-import { PageConfig } from 'remax/wechat';
-const config: PageConfig = {
+module.exports = {
   navigationBarBackgroundColor: '#fafafb',
   navigationBarTextStyle: 'black',
   navigationBarTitleText: '选择签到',
   disableScroll: true,
 };
-export default config;
