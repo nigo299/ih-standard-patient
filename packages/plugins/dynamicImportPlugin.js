2c73b69f5e844a60760089bbb9d5d3b2f18a0c65
jaluik <cqzhengmouren@gmail.com>
2023-03-13 17:59:32 +0800

    refactor: rename plugin

--- packages/plugins/dynamicImportPlugin.js
+++ packages/plugins/dynamicImportPlugin.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra');
 const path = require('path');
 
-module.exports = class TestWebpackPlugin {
+module.exports = class DynamicImportPlugin {
   constructor() {
     this.commonHisSrcPath = this.getCommonHisSrcPath();
     this.tryExt = ['ts', 'tsx'];
