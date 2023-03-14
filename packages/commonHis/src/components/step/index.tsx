0f5bf66debfd380680ead507995d5bb12e8cea36
zhangjiechun <zhangjiechun@cqkqinfo.com>
2023-03-14 15:41:07 +0800

    feat: 修改commonhis医院的组件

--- packages/commonHis/src/components/step/index.tsx
+++ packages/commonHis/src/components/step/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { navigateBack } from 'remax/one';
 import { Step } from '@kqinfo/ui';
+import { STEP_COLOR } from '@/config/constant';
+import { STEP_ITEMS } from '@/config/constant';
 
 export default (props: { step: number }) => (
   <Step
     type="dashed"
-    items={['选择院区', '选择科室', '选择医生', '选择时间']}
+    items={STEP_ITEMS}
     current={props.step}
-    defaultColor={'#80CFD6'}
+    defaultColor={STEP_COLOR}
     onChoose={(i) => {
       const delta = props.step - i;
       if (delta > 0) {
