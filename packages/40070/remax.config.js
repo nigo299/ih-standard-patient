ecb8ee913f28c11ade4affb63cd92b38bd9ba83d
jaluik <cqzhengmouren@gmail.com>
2023-04-18 14:04:27 +0800

    fix: 40070主题色调整

--- packages/40070/remax.config.js
+++ packages/40070/remax.config.js
@@ -10,7 +10,7 @@ module.exports = {
   plugins: [
     less({
       lessOptions: {
-        modifyVars: { '@brand-primary': '#CF000E' },
+        modifyVars: { '@brand-primary': '#FD92C3' },
         javascriptEnabled: true,
       },
     }),
