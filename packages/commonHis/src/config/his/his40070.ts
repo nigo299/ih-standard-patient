addb86fd9806114439ebe9b3147e214589c4c826
jaluik <cqzhengmouren@gmail.com>
2023-04-20 10:14:52 +0800

    fix: 门诊缴费

--- packages/commonHis/src/config/his/his40070.ts
+++ packages/commonHis/src/config/his/his40070.ts
@@ -1,8 +1,8 @@
 import HisBase from './hisBase';
 
-export default class His40007 extends HisBase {
+export default class His40070 extends HisBase {
   constructor() {
-    super('40007');
+    super('40070');
     this.config = Object.assign(this.config, {
       defaultAddress: '重庆市-市辖区-渝中区',
       clinicPayBatchType: 'SINGLE',
--- packages/commonHis/src/config/his/index.ts
+++ packages/commonHis/src/config/his/index.ts
@@ -1,5 +1,5 @@
 import HisBase from './hisBase';
-import His40007 from './his40007';
+import His40070 from './his40070';
 import His40009 from './his40009';
 import His2219 from './his2219';
 import { HIS_ID } from '@/config/constant';
@@ -9,9 +9,9 @@ export const getHisConfig = () => {
   if (cacheHis !== null) return cacheHis;
   const hisId = getHisId();
   switch (hisId) {
-    case '40007':
-      cacheHis = new His40007();
-      return new His40007();
+    case '40070':
+      cacheHis = new His40070();
+      return new His40070();
     case '40009':
       cacheHis = new His40009();
       return new His40009();
