a804a9b8d7addb23c143e586878b84e98ab919d0
jaluik <cqzhengmouren@gmail.com>
2023-04-24 11:51:14 +0800

    feat: 调整渝中妇幼的地址

--- packages/40070/src/config/constant.ts
+++ packages/40070/src/config/constant.ts
@@ -51,10 +51,10 @@ export const IS_FEEDBACL = PLATFORM === 'web';
 
 // https://lbs.amap.com/console/show/picker
 export const ADDRESS = {
-  longitude: 106.46,
-  latitude: 29.56,
+  longitude: 106.57164982242983,
+  latitude: 29.555445340928536,
   name: HOSPITAL_NAME,
-  address: '天陈路2号附3号世源大厦C栋',
+  address: '渝中区新民街2号',
 };
 export const WEB_ADDRESS = `https://mapapi.qq.com/web/mapComponents/locationMarker/v/index.html?marker=coord:${ADDRESS.latitude},${ADDRESS.longitude};title:${HOSPITAL_NAME};addr:${ADDRESS.address}&key=TKUBZ-D24AF-GJ4JY-JDVM2-IBYKK-KEBCU&referer=myapp&ch=uri-api&ADTAG=uri-api.myapp`;
 
