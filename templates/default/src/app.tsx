45c7c1ca5b78e096f4ea0da47ec3e55d85d23cce
jaluik <cqzhengmouren@gmail.com>
2023-04-26 14:20:17 +0800

    chore: 调整模板

--- templates/default/src/app.tsx
+++ templates/default/src/app.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import StateProviders from '@/stores';
 import { ConfigProvider } from '@kqinfo/ui';
+import eruda from 'eruda';
+import storage from '@/utils/storage';
 import './app.less';
 
 const App: React.FC = (props) => {
+  useEffect(() => {
+    if (storage.get('debugger') === 'true') {
+      eruda.init();
+    }
+  }, []);
   return (
     <StateProviders>
       <ConfigProvider brandPrimary={'#CF000E'}>
