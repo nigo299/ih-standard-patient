a2ae169ef76a61235305c37b80916e8554b8c2cf
nigo <chenzhuo@cqkqinfo.com>
2023-04-26 09:49:06 +0800

    feat: 40009修改

--- packages/commonHis/src/config/his/his40009.ts
+++ packages/commonHis/src/config/his/his40009.ts
@@ -8,7 +8,7 @@ export default class His40009 extends HisBase {
       registerDoctorTagType: 'ORIGINAL_AND_CURRENT_PRICE',
       clinicPayBatchType: 'SINGLE',
       showMedicalModal: true,
-      RegisterNoticeText:
+      registerNoticeText:
         '重要通知：重庆松山医院智慧医院界面于4月26日升级改版，使用过程中如有任何疑问请联系客服（电话：19922878076）反馈，我们将及时处理您的需求，感谢您的支持！',
     });
   }
