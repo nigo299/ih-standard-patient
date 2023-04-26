83e41c19b8f27589bdcfeaeda2be32fe1f9d71e0
nigo <chenzhuo@cqkqinfo.com>
2023-04-26 09:48:22 +0800

    feat: 40009修改

--- packages/commonHis/src/config/his/his2219.ts
+++ packages/commonHis/src/config/his/his2219.ts
@@ -5,7 +5,6 @@ export default class His2219 extends HisBase {
     super('2219');
     this.config = Object.assign(this.config, {
       defaultAddress: '重庆市-市辖区-渝中区',
-      RegisterNoticeText: '热烈庆祝重庆医科大学附属口腔医院智慧医院上线了!',
     });
   }
 }
--- packages/commonHis/src/config/his/his40070.ts
+++ packages/commonHis/src/config/his/his40070.ts
@@ -6,7 +6,6 @@ export default class His40070 extends HisBase {
     this.config = Object.assign(this.config, {
       defaultAddress: '重庆市-市辖区-渝中区',
       clinicPayBatchType: 'SINGLE',
-      RegisterNoticeText: '热烈庆祝重庆市渝中区妇幼保健院智慧医院上线了!',
     });
   }
 }
