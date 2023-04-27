c7ec691656d2545a519372ed0da14f832c330382
jaluik <cqzhengmouren@gmail.com>
2023-04-27 14:19:50 +0800

    refactor: 查询住院一日清单的extFields使用hooks

---
+++ packages/commonHis/src/pages2/inhosp/inventory/hooks/useGetExpensesDayDetailExtFields.ts
@@ -0,0 +1,10 @@
+import { InhospPatientType } from '@/apis/inhosp';
+
+interface HooksProps {
+  liveData?: InhospPatientType;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default (_: HooksProps) => {
+  return undefined;
+};
