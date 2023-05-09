8d94eaffb8fec21156bdea1b6b4e5b03cbb5ec1e
jaluik <cqzhengmouren@gmail.com>
2023-05-09 14:17:21 +0800

    feat: 增加types定义

--- packages/40070/typings/index.d.ts
+++ packages/40070/typings/index.d.ts
@@ -2,3 +2,8 @@ declare module '*.png';
 declare module '*.jpg';
 declare module '*.less';
 declare module '*.css';
+declare namespace JSX {
+  interface IntrinsicElements {
+    'wx-open-launch-weapp': any;
+  }
+}
