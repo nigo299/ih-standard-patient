73ccac0ffef01854ed2bb89594d4fbd1de0b872a
jaluik <cqzhengmouren@gmail.com>
2023-05-09 17:08:54 +0800

    feat: 增加tag错误监控

--- packages/commonHis/src/components/wxOpenLaunchWeapp/index.tsx
+++ packages/commonHis/src/components/wxOpenLaunchWeapp/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { Platform } from '@kqinfo/ui';
 import globalState from '@/stores/global';
+import { PLATFORM } from '@/config/constant';
 
 export interface IPorps {
   /** 小程序原始id */
@@ -10,10 +11,24 @@ export interface IPorps {
 }
 export default ({ path, username }: IPorps) => {
   const { initWxSDK } = globalState.useContainer();
+
+  useEffect(() => {
+    if (PLATFORM === 'web') {
+      function handler(e: any) {
+        console.error(e?.detail?.errMsg); // 无法使用开放标签的错误原因，需回退兼容。仅无法使用开放标签，JS-SDK其他功能不受影响
+      }
+      document.addEventListener('WeixinOpenTagsError', handler);
+      return () => {
+        document.removeEventListener('WeixinOpenTagsError', handler);
+      };
+    }
+  }, []);
+
   useEffect(() => {
     initWxSDK();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return (
     <Platform platform={['web']}>
       <wx-open-launch-weapp
