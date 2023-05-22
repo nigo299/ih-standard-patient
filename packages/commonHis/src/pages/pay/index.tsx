bd031202a27d631f7a151779fc85f5e49b46ca61
nigo <chenzhuo@cqkqinfo.com>
2023-05-22 21:30:10 +0800

    feat: 开发环境代理正式

--- packages/commonHis/src/pages/pay/index.tsx
+++ packages/commonHis/src/pages/pay/index.tsx
@@ -265,7 +265,8 @@ export default () => {
       if (getBrowserUa() === 'wechat') {
         const { code, msg, data } = await wechatPayRequest({
           callUrl: encodeURIComponent(
-            `${window?.location?.origin}${window?.location?.pathname}#/pages/medical/order-item/index`,
+            // `${window?.location?.origin}${window?.location?.pathname}#/pages/medical/order-item/index`,
+            `https%3A%2F%2Fihs.cqkqinfo.com%2Fpatients%2Fp40009-his-test%2F%23%2Fpages%2Fmedical%2Forder-item%2Findex`,
           ),
           bizChannel: 'insuranceWxPay',
         });
