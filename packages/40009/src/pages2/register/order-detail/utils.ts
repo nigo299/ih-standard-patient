378a6bad4a304cfa1f01cf3799d1a3c3f844641a
nigo <chenzhuo@cqkqinfo.com>
2023-06-14 14:00:54 +0800

    feat: 需求修改

--- packages/40009/src/pages2/register/order-detail/utils.ts
+++ packages/40009/src/pages2/register/order-detail/utils.ts
@@ -1,3 +1,4 @@
+export * from 'commonHis/src/pages2/register/order-detail/utils';
 export const visitTime = '报道时间';
 export const registerSuccessTips =
   '挂号成功，请根据报道时间携带绑定身份证或医保卡到科室分诊处签到候诊（最早可提前30分钟签到候诊，就诊时间以现场实际等候情况为准）,就诊卡号：';
