7ee066128370866eaf90a07610563d0b28f27cbf
nigo <chenzhuo@cqkqinfo.com>
2023-07-10 11:06:09 +0800

    feat: 40074石柱妇幼保健院

--- packages/commonHis/src/config/his/index.ts
+++ packages/commonHis/src/config/his/index.ts
@@ -1,6 +1,7 @@
 import HisBase from './hisBase';
 import His40070 from './his40070';
 import His40072 from './his40072';
+import His40074 from './his40074';
 import His40009 from './his40009';
 import His2219 from './his2219';
 import His40011 from './his40011';
@@ -26,6 +27,9 @@ export const getHisConfig = () => {
     case '40072':
       cacheHis = new His40072();
       return new His40072();
+    case '40074':
+      cacheHis = new His40074();
+      return new His40074();
     default: {
       cacheHis = new HisBase(hisId);
       return new HisBase(hisId);
