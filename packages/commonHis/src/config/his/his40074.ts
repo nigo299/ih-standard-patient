cebec7d19ae98eb5940611b3c0401ad6f7452f54
nigo <chenzhuo@cqkqinfo.com>
2023-07-10 14:07:00 +0800

    feat: 40074石柱妇幼保健院

--- packages/commonHis/src/config/his/his40074.ts
+++ packages/commonHis/src/config/his/his40074.ts
@@ -1,8 +1,8 @@
 import HisBase from './hisBase';
 
-export default class His40011 extends HisBase {
+export default class His40074 extends HisBase {
   constructor() {
-    super('40011');
+    super('40074');
     this.config = Object.assign(this.config, {
       defaultAddress: '重庆市-县-梁平区',
       clinicPayBatchType: 'SINGLE',
