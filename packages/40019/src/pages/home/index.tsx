ea7d56f3f2dfde5400f5754387d232a2f3dde406
nigo <chenzhuo@cqkqinfo.com>
2023-07-21 17:12:30 +0800

    fix: url

--- packages/40019/src/pages/home/index.tsx
+++ packages/40019/src/pages/home/index.tsx
@@ -253,7 +253,7 @@ export default () => {
         title: '健康宣教',
         subTitle: '',
         image: `${IMAGE_DOMIN}/home/jkxj.png`,
-        url: '/pages/microsite/hospital-article/index?type=140&title=%E5%81%A5%E5%BA%B7%E5%AE%A3%E6%95%99',
+        url: '/pages/microsite/hospital-article/index?type=156&title=%E5%81%A5%E5%BA%B7%E5%AE%A3%E6%95%99',
       },
       {
         title: '意见反馈',
