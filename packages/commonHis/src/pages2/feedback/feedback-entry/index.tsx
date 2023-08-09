ad410ce1b3c7af27904baf796fbaf65b1c0518eb
nigo <chenzhuo@cqkqinfo.com>
2023-08-09 17:22:13 +0800

    feat: 问卷

--- packages/commonHis/src/pages2/feedback/feedback-entry/index.tsx
+++ packages/commonHis/src/pages2/feedback/feedback-entry/index.tsx
@@ -1,26 +1,11 @@
-import React, { useState } from 'react';
-import { IMAGE_DOMIN, IS_FEEDBACL } from '@/config/constant';
-import {
-  Button,
-  Icon,
-  // Loading,
-  PartTitle,
-  previewImage,
-  ReTextarea,
-  showToast,
-  Space,
-} from '@kqinfo/ui';
-import { View, Image, Text, navigateBack } from '@remax/one';
-
+import React from 'react';
+import { View } from '@remax/one';
 import useGetParams from '@/utils/useGetParams';
 import setNavigationBar from '@/utils/setNavigationBar';
 import { usePageEvent } from 'remax/macro';
-import globalState from '@/stores/global';
-import styles from '@/pages2/feedback/feedback-add/index.less';
 import IPD from './component/IPD';
 import OPD from './component/OPD';
 export default () => {
-  const { initWxSDK } = globalState.useContainer();
   const { type = 'IPD' } = useGetParams<{
     type: string;
     deptName: string;
@@ -30,7 +15,6 @@ export default () => {
   }>();
 
   usePageEvent('onShow', () => {
-    initWxSDK();
     setNavigationBar({
       title: '意见反馈',
     });
