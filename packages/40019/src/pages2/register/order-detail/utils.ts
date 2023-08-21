30ffa619716b50bd3921a061e7ec27c7da8bf940
nigo <chenzhuo@cqkqinfo.com>
2023-08-21 10:08:57 +0800

    feat: 修改梁平中挂号详情展示的卡号信息

--- packages/40019/src/pages2/register/order-detail/utils.ts
+++ packages/40019/src/pages2/register/order-detail/utils.ts
@@ -1,4 +1,4 @@
 export const visitTime = '就诊时间';
 export const registerSuccessTips =
   '挂号成功，请根据签到时间至少提前20分钟携带绑定的卡（身份证、医保卡、院内诊疗卡）到科室分诊处签到候诊。';
-export const RegisterCardPatientNo = 'patCardNo';
+export const RegisterCardPatientNo = 'patHisNo';
