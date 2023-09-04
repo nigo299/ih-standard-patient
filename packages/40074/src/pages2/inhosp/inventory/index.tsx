5ace11b838c2d9e4ae4231a01feac770c0b963f2
nigo <chenzhuo@cqkqinfo.com>
2023-09-04 12:48:23 +0800

    feat: update and fix

--- packages/40074/src/pages2/inhosp/inventory/index.tsx
+++ packages/40074/src/pages2/inhosp/inventory/index.tsx
@@ -6,7 +6,7 @@ import { Space, BackgroundImg, Picker, Icon, Table } from '@kqinfo/ui';
 import { IMAGE_DOMIN } from '@/config/constant';
 import useApi from '@/apis/inhosp';
 import dayjs from 'dayjs';
-import styles from './index.less';
+import styles from 'commonHis/src/pages2/inhosp/inventory/index.less';
 import reportCmPV from '@/alipaylog/reportCmPV';
 import useGetParams from '@/utils/useGetParams';
 import { PatGender } from '@/config/dict';
@@ -81,6 +81,7 @@ export default () => {
               </Space>
               <Space alignItems="center" className={styles.date}>
                 <Picker
+                  value={selectDate}
                   mode={'date'}
                   onChange={(v) => setSelectDate(String(v))}
                   start={liveData?.inDate.slice(0, 10)}
