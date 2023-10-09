71d9ad86b8fd629c8f0a0e158c9e291f4ed3b23e
nigo <15523362813cz@gmail.com>
2023-10-09 17:03:03 +0800

    feat: 文字修改

--- packages/commonHis/src/config/his/his40009.ts
+++ packages/commonHis/src/config/his/his40009.ts
@@ -12,7 +12,7 @@ export default class His40009 extends HisBase {
       showCalenderInTodayRegister: false,
       showSelectTypeInTodayRegister: false,
       registerNoticeText:
-        '重要通知：即日起-2023年12月31日（工作日下午及周末）门诊普通诊察费免费，专家诊察费5折优惠；60岁以上人群享受全天门诊普通诊察费免费，专家诊察费5折优惠。',
+        '即日起至2023年12月31日，60岁以上人群普通门诊免挂号费，专家门诊5折优惠；其余人群下午及周末普通门诊免费，专家门诊5折优惠（以上活动限自费）。',
     });
   }
 }
