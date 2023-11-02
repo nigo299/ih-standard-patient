0b1c1659390b129acc691f41950bf158252ff279
copyer <805141136@qq.com>
2023-11-02 10:25:54 +0800

    feat: 删除无用代码

--- packages/40009/src/pages4/booking/team/components/listItem.tsx
+++ packages/40009/src/pages4/booking/team/components/listItem.tsx
@@ -24,61 +24,6 @@ const Item: React.FC<Props> = (props) => {
   return (
     <View className={styles.box}>
       <Shadow>
-        <ListItem
-          className={styles.itemBox}
-          img={<PreviewImage url={content.avatarImage}></PreviewImage>}
-          imgFooterCls={styles.imgFooter}
-          imgFooter={
-            <View className={styles.imgFooter}>
-              {[MODE.线下, MODE.全部].includes(content.mode) && (
-                <Text className={`${styles.txt} ${styles.top}`}>线下会诊</Text>
-              )}
-              {[MODE.线上, MODE.全部].includes(content.mode) && (
-                <Text className={`${styles.txt} ${styles.bottom}`}>
-                  线上会诊
-                </Text>
-              )}
-            </View>
-          }
-          title={<Text className={styles.title}>{content.teamName}</Text>}
-          text={
-            <View>
-              <Text className={styles.commonTxt}>{content.hospitalName}</Text>
-              <Exceed className={styles.commonTxt} clamp={1}>
-                {content.intro}
-              </Exceed>
-            </View>
-          }
-          footer={
-            <View className={styles.footer}>
-              <Text className={`${styles.commonColor} ${styles.price}`}>
-                {' '}
-                ￥{Number(content.price) / 100}/次
-              </Text>
-              <Button
-                type={'primary'}
-                block={false}
-                size="small"
-                onTap={() => handleTap(content.id)}
-              >
-                预约会诊
-              </Button>
-            </View>
-          }
-        ></ListItem>
-      </Shadow>
-    </View>
-  );
-};
-
-const NewItem: React.FC<Props> = (props) => {
-  const { content } = props;
-  const handleTap = (teamId: string) => {
-    navigateTo({ url: `/pages4/booking/team/detail?teamId=${teamId}` });
-  };
-  return (
-    <View className={styles.box}>
-      <Shadow>
         <Space size={20} className={styles.itemBox}>
           <Space vertical>
             <PreviewImage
@@ -123,4 +68,4 @@ const NewItem: React.FC<Props> = (props) => {
   );
 };
 
-export default NewItem;
+export default Item;
