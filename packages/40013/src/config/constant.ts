e7dfce63687a020d7df6c8aa69afa70414b7e2ef
copyer <805141136@qq.com>
2023-12-11 11:42:57 +0800

    feat: add icon

--- packages/40013/src/config/constant.ts
+++ packages/40013/src/config/constant.ts
@@ -11,12 +11,12 @@ export const IMAGE_DOMIN = `${
     : 'https://tihs.cqkqinfo.com/patients/p40064-his/images'
 }`;
 export const STEP_ITEMS = ['选择院区', '选择科室', '选择医生', '选择时间'];
-export const HOSPITAL_NAME = '重庆沙坪坝区妇幼保健院';
-export const HIS_ID = '40064';
+export const HOSPITAL_NAME = '大足妇幼';
+export const HIS_ID = '40013';
 export const HOSPITAL_TEL = '023-43780184';
 export const REQUEST_QUERY = {
-  hisId: 40064,
-  platformId: PLATFORM === 'ali' ? 4006402 : 4006401,
+  hisId: 40013,
+  platformId: PLATFORM === 'ali' ? 4001302 : 4001301,
   platformSource:
     process.env.REMAX_APP_PLATFORM === 'app'
       ? 10
