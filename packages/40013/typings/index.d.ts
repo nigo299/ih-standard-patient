9ee167b2f1ad0fff5c7f10fb624c672c5d75d91c
copyer <805141136@qq.com>
2023-12-11 11:14:20 +0800

    feat:add

---
+++ packages/40013/typings/index.d.ts
@@ -0,0 +1,9 @@
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.less';
+declare module '*.css';
+declare namespace JSX {
+  interface IntrinsicElements {
+    'wx-open-launch-weapp': any;
+  }
+}
