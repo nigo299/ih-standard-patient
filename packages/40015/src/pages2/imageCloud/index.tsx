ecda947c4fa713a62a0e9c9c237a5bfae832cfbd
copyer <805141136@qq.com>
2023-12-19 15:22:26 +0800

    feat: 测试

--- packages/40015/src/pages2/imageCloud/index.tsx
+++ packages/40015/src/pages2/imageCloud/index.tsx
@@ -1,39 +1,39 @@
 import React, { useEffect } from 'react';
-import { View, navigateTo } from 'remax/one';
-import { usePageEvent } from 'remax/macro';
+import { View } from 'remax/one';
+// import { usePageEvent } from 'remax/macro';
 import { Loading } from '@kqinfo/ui';
-import patientState from '@/stores/patient';
+// import patientState from '@/stores/patient';
 import useApi from '@/apis/common';
-import { decrypt } from '@/utils';
+// import { decrypt } from '@/utils';
 import useGetParams from '@/utils/useGetParams';
 
 export default () => {
   const { patientId } = useGetParams<{
     patientId: string;
   }>();
-  const { setFaceInfo, defaultPatientInfo, faceInfo } =
-    patientState.useContainer();
-  const { data, loading, request } = useApi.获取应用管理系统授权页({
+  // const { setFaceInfo, defaultPatientInfo, faceInfo } =
+  //   patientState.useContainer();
+  const { data, loading } = useApi.获取应用管理系统授权页({
     params: {
       patientId,
     },
-    needInit: false,
+    needInit: true,
   });
-  usePageEvent('onLoad', () => {
-    setFaceInfo({
-      idNo: decrypt(defaultPatientInfo?.encryptIdNo as string) as string,
-      name: decrypt(defaultPatientInfo?.encryptPatientName as string) as string,
-      success: false,
-    });
-    navigateTo({
-      url: '/pages2/usercenter/face-verify/index',
-    });
-  });
-  useEffect(() => {
-    if (faceInfo.success) {
-      request({ patientId });
-    }
-  }, [faceInfo.success, patientId, request]);
+  // usePageEvent('onLoad', () => {
+  //   setFaceInfo({
+  //     idNo: decrypt(defaultPatientInfo?.encryptIdNo as string) as string,
+  //     name: decrypt(defaultPatientInfo?.encryptPatientName as string) as string,
+  //     success: false,
+  //   });
+  //   navigateTo({
+  //     url: '/pages2/usercenter/face-verify/index',
+  //   });
+  // });
+  // useEffect(() => {
+  //   if (faceInfo.success) {
+  //     request({ patientId });
+  //   }
+  // }, [faceInfo.success, patientId, request]);
   useEffect(() => {
     if (data?.data?.indexUrl) {
       window.location.href = data.data?.indexUrl;
